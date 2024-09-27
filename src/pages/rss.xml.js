1240b3629e9fd369ae99f1916e8de4e7180eae96
Mzagal <mzagaltapia@hotmail.com>
2024-09-27 10:42:59 -0300

    rrss, frameworks y posts

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,12 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: 'Alumno de Astro | Blog',
+    description: 'Mi viaje de aprendizaje de Astro',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: '<language>es-cl</language>'
+    // customData: `<language>es-cl</language>`,
+  })
+}
